11fe3afedeee6e9ff972f893b8caf92a63b14568
Pablo <pablo.zapata.dev@gmail.com>
2021-04-18 17:55:17 +0200

    ok

--- modules/jobsManager/sqlchemes/ojap.sql
+++ modules/jobsManager/sqlchemes/ojap.sql
@@ -2,4 +2,3 @@ INSERT INTO `jobs` (`name`, `label`, `whitelisted`, `usePabloSystem`) VALUES ('o
 INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES (NULL, 'ojap', '0', 'recruit', 'Recrue', '150', '{}', '{}'), (NULL, 'ojap', '1', 'member', 'Employé', '1500', '{}', '{}'), (NULL, 'ojap', '2', 'boss', 'Patron', '1', '{}', '{}');
 INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES ('society_ojap', 'O\'Jap', '1');
 INSERT INTO `addon_account_data` (`id`, `account_name`, `money`, `owner`) VALUES (NULL, 'society_ojap', '10000', NULL);
-INSERT INTO `addon_inventory` (`id`, `name`, `label`, `shared`) VALUES (NULL, 'society_ojap', 'O\'Jap', '1');
