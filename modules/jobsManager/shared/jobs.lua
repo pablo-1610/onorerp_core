0e3702fb37c8c09ed1ca9d5cd2c78b7219bd7047
PABLO-1610 <pabzapata16@gmail.com>
2021-04-25 03:39:05 +0200

    Modif miss

--- modules/jobsManager/shared/jobs.lua
+++ modules/jobsManager/shared/jobs.lua
@@ -84,20 +84,20 @@ OnoreSharedCustomJobs = {
             job:registerAdditionalZone(foodZone)
         end,
 
-        inventory = vector3(-437.14, -35.20, 39.97),
-        laundry = vector3(-446.28, -24.79, 39.97),
-        boss = vector3(-441.79, -38.18, 39.97),
+        inventory = vector3(-437.21, -35.14, 40.17),
+        laundry = vector3(-446.28, -24.79, 40.17),
+        boss = vector3(-441.79, -38.18, 40.17),
         -- Add garage
 
         clothes = {
             ["boss"] = {
                 ["M"] = {
-                    ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
-                    ['torso_1'] = 107,   ['torso_2'] = 0,
-                    ['arms'] = 6,
-                    ['pants_1'] = 24,   ['pants_2'] = 5,
-                    ['shoes_1'] = 16,   ['shoes_2'] = 10,
-                    ['helmet_1'] = 6,  ['helmet_2'] = 7,
+                    ['tshirt_1'] = 72,  ['tshirt_2'] = 3,
+                    ['torso_1'] = 31,   ['torso_2'] = 0,
+                    ['arms'] = 17,
+                    ['pants_1'] = 24,   ['pants_2'] = 0,
+                    ['shoes_1'] = 10,   ['shoes_2'] = 0,
+                    ['helmet_1'] = 7,  ['helmet_2'] = 0,
                     ['chain_1'] = 0,    ['chain_2'] = 0,
                 },
 
@@ -108,18 +108,17 @@ OnoreSharedCustomJobs = {
                     ['pants_1'] = 6,   ['pants_2'] = 0,
                     ['shoes_1'] = 5,   ['shoes_2'] = 0,
                     ['helmet_1'] = 4,  ['helmet_2'] = 7,
-                    ['chain_1'] = 0,    ['chain_2'] = 0,
-
+                    ['chain_1'] = 0,    ['chain_2'] = 0
                 }
             },
             ["member"] = {
                 ["M"] = {
-                    ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
-                    ['torso_1'] = 107,   ['torso_2'] = 0,
-                    ['arms'] = 6,
-                    ['pants_1'] = 24,   ['pants_2'] = 5,
-                    ['shoes_1'] = 16,   ['shoes_2'] = 10,
-                    ['helmet_1'] = 6,  ['helmet_2'] = 7,
+                    ['tshirt_1'] = 72,  ['tshirt_2'] = 3,
+                    ['torso_1'] = 31,   ['torso_2'] = 0,
+                    ['arms'] = 17,
+                    ['pants_1'] = 24,   ['pants_2'] = 0,
+                    ['shoes_1'] = 10,   ['shoes_2'] = 0,
+                    ['helmet_1'] = 7,  ['helmet_2'] = 0,
                     ['chain_1'] = 0,    ['chain_2'] = 0,
                 },
 
@@ -136,12 +135,12 @@ OnoreSharedCustomJobs = {
             },
             ["recruit"] = {
                 ["M"] = {
-                   ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
-                    ['torso_1'] = 107,   ['torso_2'] = 0,
+					['tshirt_1'] = 22,  ['tshirt_2'] = 0,
+                    ['torso_1'] = 11,   ['torso_2'] = 1,
                     ['arms'] = 6,
-                    ['pants_1'] = 24,   ['pants_2'] = 5,
-                    ['shoes_1'] = 16,   ['shoes_2'] = 10,
-                    ['helmet_1'] = 6,  ['helmet_2'] = 7,
+                    ['pants_1'] = 24,   ['pants_2'] = 0,
+                    ['shoes_1'] = 10,   ['shoes_2'] = 0,
+                    ['helmet_1'] = 7,  ['helmet_2'] = 0,
                     ['chain_1'] = 0,    ['chain_2'] = 0,
                 },
 
